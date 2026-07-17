f890f5398baed81c6f43b4af6fe2764bc07fd630
unicodick <vndljs@gmail.com>
2026-07-17 18:56:11 +0300

    feat(home): link creator profiles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Skin from "$lib/components/Skin.svelte";
   import { CREATORS, BRAND, skinRender } from "$lib/creators";
+  import { playerProfilePath } from "$lib/player-paths";
 </script>
 
 <svelte:head>
@@ -13,7 +14,14 @@
 >
   <div class="flex items-end justify-center gap-6 sm:gap-16">
     {#each CREATORS as creator (creator.name)}
-      <Skin name={creator.name} src={skinRender(creator.name)} />
+      <a
+        href={playerProfilePath(creator.name)}
+        class="block outline-none focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-grass-dim"
+        aria-label={`Профиль игрока ${creator.name}`}
+        title={creator.name}
+      >
+        <Skin name={creator.name} src={skinRender(creator.name)} />
+      </a>
     {/each}
   </div>
 </main>
